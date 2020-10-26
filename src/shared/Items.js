515486dc6c9a13d24b63c2692ae74d8a00ed876d
gh-oss-contributor <72963869+gh-oss-contributor@users.noreply.github.com>
2020-10-26 11:55:38 +0530

    Adds one apple to the basket
    Co-authored-by: gh-oss-contributor <72963869+gh-oss-contributor@users.noreply.github.com>
    GITHUB_PR_NUMBER: 52
    GITHUB_PR_URL: https://github.com/sample-organisation/h-oss/pull/52
    GitOrigin-RevId: cfd8790124a6f87eddac72c5c8a761781b2a49e2

--- src/shared/Items.js
+++ src/shared/Items.js
@@ -1,10 +1,11 @@
 import Basket from './Basket';
+import Apple from './Apple';
 
 function Items() {
   return (
     <div className="flex content-center flex-wrap h-48">
       <div className="w-1/4 p-2">
-        <Basket />
+        <Apple />
       </div>
       <div className="w-1/4 p-2">
         <div className="text-6xl text-center p-2">
