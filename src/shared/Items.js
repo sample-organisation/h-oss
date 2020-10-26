30c7b4a208cb6075f5848786d862660fec128719
gh-oss-contributor <72963869+gh-oss-contributor@users.noreply.github.com>
2020-10-26 17:50:50 +0530

    Update Items.js

--- src/shared/Items.js
+++ src/shared/Items.js
@@ -5,16 +5,16 @@ function Items() {
   return (
     <div className="flex content-center flex-wrap h-48">
       <div className="w-1/4 p-2">
-        <Basket />
-      </div>
-      <div className="w-1/4 p-2">
-        <Basket />
-      </div>
-      <div className="w-1/4 p-2">
-        <Basket />
-      </div>
-      <div className="w-1/4 p-2">
         <Apple />
+      </div>
+      <div className="w-1/4 p-2">
+        <Basket />
+      </div>
+      <div className="w-1/4 p-2">
+        <Basket />
+      </div>
+      <div className="w-1/4 p-2">
+        <Basket />
       </div>
     </div>
   )
